db68f8424e84b01f9f5ab118dbba3ac77926ad7f
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2026-01-22 16:12:59 -0500

    Completed #27 Remove Element leet code question.

---
+++ remove-element.js
@@ -0,0 +1,25 @@
+//January 22, 2026 #27 Remove Element
+
+/* INSTRUCTIONS
+Given an integer array and a value remove all occurrences of the value in the array IN-PLACE. 
+The order of elements may be changed.
+Return the number of elements in the array which are not equal to val 
+*/
+
+let nums = [3,2,2,3]; //we should expect to see a return of 4 elements once both 3's are removed.
+let val = 3;
+
+function removeElements(nums,val){
+    let k = 0
+
+    for(let i=0; i <nums.length; i++){
+        if(nums[i] !== val){
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k
+}
+
+console.log(removeElements(nums,val))
