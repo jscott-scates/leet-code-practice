3c3d12fdbeb5865200bdd8718019a72c12351684
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2026-01-30 15:57:07 -0500

    Completed #189. Rotate Array

--- arraysAndStrings/remove-duplicates-from-sorted-array-2.js
+++ arraysAndStrings/remove-duplicates-from-sorted-array-2.js
@@ -12,8 +12,8 @@ let nums = [0,0,1,1,1,1,2,3,3];
 var removeDuplicates = function(nums) {
     let k = 2;
 
-    for (let i=2; i<nums.length; i++){
-        if(nums[i] !== nums[k-2]){
+    for (let i = 2; i < nums.length; i++){
+        if (nums[i] !== nums[k-2]){
             nums[k] = nums[i];
             k++;
         }
